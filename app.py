ad4749ad6e53866465bf90e9c70a36c05ce9d7e6
Pallavi Shivappa <pallavishivappagm@gmail.com>
2026-05-10 23:02:02 +0530

    Updated gitignore

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ def health():
 
 # ---------------- START LOCAL ----------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port, debug=False)
